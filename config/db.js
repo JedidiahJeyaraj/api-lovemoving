5eac1dc751091d9bd99f82be17aa3f98151ab509
Umakant B <umakant.b@2adpro.com>
2018-11-10 16:35:59 +0530

    client and customer dashboard

--- config/db.js
+++ config/db.js
@@ -3,25 +3,11 @@ var mysql = require('mysql')
 
 var appconfig = {};
 
-// appconfig.getConnection = function(){
-//     var connection = mysql.createConnection({
-//     host     : 'db4free.net',
-//     user     : 'umakant',
-//     password : 'umakantmane',
-//     database : 'lovemoving',
-//     port:3306
-//     });
-//     connection.connect();
-
-//     return connection;
-// };
-
-
 appconfig.getConnection = function(){
     var connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : 'db4free.net',
     user     : 'umakant',
-    password : 'welcome@123',
+    password : 'umakantmane',
     database : 'lovemoving',
     port:3306
     });
@@ -29,6 +15,20 @@ appconfig.getConnection = function(){
 
     return connection;
 };
+
+
+// appconfig.getConnection = function(){
+//     var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'umakant',
+//     password : 'welcome@123',
+//     database : 'lovemoving',
+//     port:3306
+//     });
+//     connection.connect();
+
+//     return connection;
+// };
 
 
 module.exports = appconfig;
